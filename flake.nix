ccae591243b7abf9399f9d5ef14dd10f9d1c5f2a
James Pakele <james@pakele.dev>
2026-09-17 21:55:52 -1000

    Initial flake: niri + noctalia VM host

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "NixOS config (niri + noctalia)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    noctalia = {
+      url = "github:noctalia-dev/noctalia/cachix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, noctalia }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/vm/configuration.nix
+          noctalia.nixosModules.default
+        ];
+      };
+    };
+}
---
+++ hosts/vm/configuration.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, ... }:
+
+{
+  nix.settings = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "vm";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Pacific/Honolulu";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # niri compositor
+  programs.niri.enable = true;
+
+  # noctalia shell (package + recommended services: NetworkManager, BT, UPower, power profiles)
+  programs.noctalia = {
+    enable = true;
+    recommendedServices.enable = true;
+    systemd.enable = true;
+  };
+
+  # greetd with niri session (noctalia-greeter optional later)
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd niri-session";
+        user = "greeter";
+      };
+    };
+  };
+
+  # VM niceties
+  services.qemuGuest.enable = true;
+  services.spice-vdagentd.enable = true;
+
+  users.users.pakele = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    initialPassword = "changeme"; # replace with hashed password after first boot
+  };
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    curl
+    foot
+  ];
+
+  system.stateVersion = "26.05";
+}
